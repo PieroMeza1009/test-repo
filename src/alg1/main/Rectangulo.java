79d6e958d95dff4c1600deef4923a78687634cad
PieroMeza10 <pieroms0905@gmail.com>
2025-03-23 22:34:34 -0500

    CAMBIO R2

--- src/alg1/main/Rectangulo.java
+++ src/alg1/main/Rectangulo.java
@@ -7,6 +7,14 @@ public class Rectangulo {
     public Rectangulo(Coordenada c1, Coordenada c2) {
         setEsquina1(c1);
         setEsquina2(c2);
-    }   
+    }
+    
+    public void setEsquina1(Coordenada coo) {
+        this.esquina1 = new Coordenada(coo); 
+    }
+
+    public void setEsquina2(Coordenada coo) {
+        this.esquina2 = new Coordenada(coo); // Se crea una copia para evitar aliasing
+    }
 
 }
