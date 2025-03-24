e475c083ad8efa754109485987a5a8a5b8ad6a7a
PieroMeza10 <pieroms0905@gmail.com>
2025-03-23 21:11:23 -0500

    agregado hola mundo

--- src/alg1/main/Coordenada.java
+++ src/alg1/main/Coordenada.java
@@ -3,6 +3,6 @@ package alg1.main;
 public class Coordenada {
 
     public static void main(String[] args) {
-        System.out.println("intento bueno");
+        System.out.println("hola mundo");
     }
 }
