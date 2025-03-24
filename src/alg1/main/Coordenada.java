181a030a112f6856a8cac3ea30a9a79516029fe4
PieroMeza10 <pieroms0905@gmail.com>
2025-03-23 21:16:52 -0500

    cambio 2

--- src/alg1/main/Coordenada.java
+++ src/alg1/main/Coordenada.java
@@ -3,6 +3,6 @@ package alg1.main;
 public class Coordenada {
 
     public static void main(String[] args) {
-        System.out.println("hola mundo");
+        System.out.println("aqui esta el cambio 3");
     }
 }
