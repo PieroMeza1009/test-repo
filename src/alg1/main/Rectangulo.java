4f353bcd67993d9619cf6f572fefe0ae53355423
PieroMeza10 <pieroms0905@gmail.com>
2025-03-23 22:42:15 -0500

    CAMBIO R4

--- src/alg1/main/Rectangulo.java
+++ src/alg1/main/Rectangulo.java
@@ -16,12 +16,18 @@ public class Rectangulo {
     public void setEsquina2(Coordenada coo) {
         this.esquina2 = new Coordenada(coo); 
     }
+
     public Coordenada getEsquina1() {
         return new Coordenada(this.esquina1); 
     }
 
     public Coordenada getEsquina2() {
         return new Coordenada(this.esquina2);
+    }
+    
+    @Override
+    public String toString() {
+        return "Rectángulo formado por las esquinas " + esquina1.toString() + " y " + esquina2.toString();
     }
 
 }
