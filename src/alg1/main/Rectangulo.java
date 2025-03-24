6fbeedf8cff2e5af0edc7c57cf0e69ff50d010b8
PieroMeza10 <pieroms0905@gmail.com>
2025-03-23 22:32:48 -0500

    cambio R1

---
+++ src/alg1/main/Rectangulo.java
@@ -0,0 +1,12 @@
+package alg1.main;
+
+public class Rectangulo {
+    private Coordenada esquina1;
+    private Coordenada esquina2;
+
+    public Rectangulo(Coordenada c1, Coordenada c2) {
+        setEsquina1(c1);
+        setEsquina2(c2);
+    }   
+
+}
