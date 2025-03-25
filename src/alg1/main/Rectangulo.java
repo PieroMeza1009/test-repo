c87b09193db09565626a36d5673a3fbc59157be0
PieroMeza10 <pieroms0905@gmail.com>
2025-03-24 22:54:52 -0500

    CAMBIO R6

--- src/alg1/main/Rectangulo.java
+++ src/alg1/main/Rectangulo.java
@@ -5,9 +5,16 @@ public class Rectangulo {
     private Coordenada esquina2;
 
     public Rectangulo(Coordenada c1, Coordenada c2) {
-        setEsquina1(c1);
-        setEsquina2(c2);
+        double xMin = Math.min(c1.getX(), c2.getX());
+        double yMin = Math.min(c1.getY(), c2.getY());
+        double xMax = Math.max(c1.getX(), c2.getX());
+        double yMax = Math.max(c1.getY(), c2.getY());
+
+        this.esquina1 = new Coordenada(xMin, yMin); // Inferior izquierda
+        this.esquina2 = new Coordenada(xMax, yMax); // Superior derecha
     }
+
+
 
     public void setEsquina1(Coordenada coo) {
         this.esquina1 = new Coordenada(coo); 
