265b3716c69b3221a1686c49f3143020d9325385
PieroMeza10 <pieroms0905@gmail.com>
2025-03-25 13:49:04 -0500

    CAMBIO P4

--- src/alg1/main/Principal.java
+++ src/alg1/main/Principal.java
@@ -2,14 +2,15 @@ package alg1.main;
 
 import java.util.Scanner;
 
+//clase que ejecuta el programa
 public class Principal {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); // creo un objeto Scanner para leer datos del teclado
 
         System.out.println("Ingrese las coordenadas del primer rectángulo (x1 y1 x2 y2):");
-        Coordenada c1 = new Coordenada(sc.nextDouble(), sc.nextDouble());
-        Coordenada c2 = new Coordenada(sc.nextDouble(), sc.nextDouble());
-        Rectangulo rectA = new Rectangulo(c1, c2);
+        Coordenada c1 = new Coordenada(sc.nextDouble(), sc.nextDouble()); //lee las coordenadas x1, y1, x2, y2 usando sc.nextDouble
+        Coordenada c2 = new Coordenada(sc.nextDouble(), sc.nextDouble()); // método de la clase Scanner en Java que lee un número decimal
+        Rectangulo rectA = new Rectangulo(c1, c2); //creo un objeto Rectangulo llamado rectA usando c1 y c2
 
         System.out.println("Ingrese las coordenadas del segundo rectángulo (x1 y1 x2 y2):");
         Coordenada c3 = new Coordenada(sc.nextDouble(), sc.nextDouble());
@@ -19,9 +20,10 @@ public class Principal {
         mostrarRectangulo(rectA, "A");
         mostrarRectangulo(rectB, "B");
 
+        //lama al método sobreponen de la clase Verificador
         if (Verificador.sobreponen(rectA, rectB)) {
             System.out.println("Los rectángulos A y B se sobreponen.");
-            Rectangulo areaSobre = calcularSobreposicion(rectA, rectB);
+            Rectangulo areaSobre = calcularSobreposicion(rectA, rectB); //calcula el área de la intersección llamando a calcularSobreposicion
             System.out.println("Área de sobreposición: " + areaSobre.calculoArea());
         } else if (Verificador.tocan(rectA, rectB)) {
             System.out.println("Los rectángulos A y B están juntos.");
@@ -32,12 +34,12 @@ public class Principal {
     }
     
     public static void mostrarRectangulo(Rectangulo r, String nombre) {
-        System.out.println("Rectángulo " + nombre + ": " + r);
+        System.out.println("Rectángulo " + nombre + ": " + r); // Imprime la información del rectángulo usando toString
     }
     public static Rectangulo calcularSobreposicion(Rectangulo A, Rectangulo B) {
-        double x1 = Math.max(A.getEsquina1().getX(), B.getEsquina1().getX());
+        double x1 = Math.max(A.getEsquina1().getX(), B.getEsquina1().getX());    //Uso Math.max para encontrar la esquina inferior izquierda del área de sobreposición
         double y1 = Math.max(A.getEsquina1().getY(), B.getEsquina1().getY());
-        double x2 = Math.min(A.getEsquina2().getX(), B.getEsquina2().getX());
+        double x2 = Math.min(A.getEsquina2().getX(), B.getEsquina2().getX());    //Uso Math.min para encontrar la esquina superior derecha.
         double y2 = Math.min(A.getEsquina2().getY(), B.getEsquina2().getY());
 
         return new Rectangulo(new Coordenada(x1, y1), new Coordenada(x2, y2));
