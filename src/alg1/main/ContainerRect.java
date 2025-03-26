53df8b3fa01b47f870c1cea95f8f7af1ca031229
PieroMeza10 <pieroms0905@gmail.com>
2025-03-25 21:28:54 -0500

    CAMBIO CR1

---
+++ src/alg1/main/ContainerRect.java
@@ -0,0 +1,10 @@
+package alg1.main;
+
+public class ContainerRect {
+    private Rectangulo[] rectangulos;
+    private double[] distancias;
+    private double[] areas;
+    private int n;
+    private static int numRec = 0;
+
+}
