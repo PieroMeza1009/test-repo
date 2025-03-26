0fa27f1f18e9825b88c2f42ecf5b5a4def00ae75
PieroMeza10 <pieroms0905@gmail.com>
2025-03-25 21:32:09 -0500

    CAMBIO CR3

--- src/alg1/main/ContainerRect.java
+++ src/alg1/main/ContainerRect.java
@@ -13,5 +13,15 @@ public class ContainerRect {
         this.distancias = new double[n];
         this.areas = new double[n];
     }
+    
+    public void addRectangulo(Rectangulo r) {
+        if (numRec < n) {
+            rectangulos[numRec] = r;
+            distancias[numRec] = r.getEsquina1().distancia(r.getEsquina2());
+            areas[numRec] = r.calculoArea();
+            numRec++;
+        } else {
+            System.out.println("No se pueden agregar más rectángulos. Capacidad máxima alcanzada.");
+        }
 
 }
